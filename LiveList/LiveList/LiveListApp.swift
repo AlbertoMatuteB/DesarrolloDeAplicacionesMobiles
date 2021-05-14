af5adb46f187d9c71ca85c6502ec6908e3145d57
Alberto Matute Beltran <RodrigoMB@MacBook-12.local>
2021-05-13 21:26:34 -0500

    avance Lab

---
+++ LiveList/LiveList/LiveListApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveListApp.swift
+//  LiveList
+//
+//  Created by RODRIGO MATUTE BELTRAN on 13/05/21.
+//
+
+import SwiftUI
+
+@main
+struct LiveListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
